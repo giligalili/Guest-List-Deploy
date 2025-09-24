52744da09ced13c0db5860896035a8fab380850c
Sivan Monshi <sivan.monshi@gmail.com>
2025-09-24 15:08:33 +0300

    Update backend for Sivan's state management

--- backend.tf
+++ backend.tf
@@ -1,19 +1,9 @@
 terraform {
-  required_version = ">= 1.0.0, < 1.10.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
-
   backend "s3" {
-    bucket               = "guestlist-tfstate-bucket"
-    key                  = "terraform.tfstate"   # key קצר; ה-workspace יתווסף בנתיב
-    region               = "us-east-1"
-    encrypt              = true
-    dynamodb_table       = "terraform-locks"     # נעילה בטוחה
-    workspace_key_prefix = "envs"                # state יישמר תחת: envs/<workspace>/terraform.tfstate
+    bucket         = "guestlist-tfstate-sivan"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks-sivan"
+    encrypt        = true
   }
 }
